d1454e8e55e2f385eca1e47ee8a883390bd2a639
jose-H-M <53890404+jose-H-M@users.noreply.github.com>
2020-06-29 18:35:00 -0600

    Agrego archivos

---
+++ INCISO_13.sql
@@ -0,0 +1,4 @@
+--ACTUALIZAR NUMERO DE TELEFONO
+UPDATE MIEMBRO SET telefono=55464601 WHERE CONCAT(nombre,' ',apellido)='Laura Cunha Silva';
+UPDATE MIEMBRO SET telefono=91514243 WHERE CONCAT(nombre,' ',apellido)='Jeuel Villalpando';
+UPDATE MIEMBRO SET telefono=920686670 WHERE CONCAT(nombre,' ',apellido)='Scott Mitchell';
